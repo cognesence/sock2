202c067770e395b161c5c336c17f1b70909b70a2
Saul Johnson <saul.a.johnson@gmail.com>
2017-06-30 15:00:02 +0100

    Reformat file

--- src/SocketExtension.java
+++ src/SocketExtension.java
@@ -10,7 +10,8 @@ public class SocketExtension extends org.nlogo.api.DefaultClassManager
 {
     private static NetlogoSocketIOStub comm;
     
-    public void load(org.nlogo.api.PrimitiveManager primManager) {
+    @Override
+    public void load(PrimitiveManager primManager) {
         primManager.addPrimitive("connect-local", new ConnectLocal());
         primManager.addPrimitive("connect-distant", new ConnectDist());
 	primManager.addPrimitive("serve-socket", new Advertise());
@@ -22,252 +23,272 @@ public class SocketExtension extends org.nlogo.api.DefaultClassManager
 	primManager.addPrimitive("read", new Take());
 	primManager.addPrimitive("write", new Write());
 	primManager.addPrimitive("is-empty", new IsEmpty());
-  }
+    }
 
-
-  //===========================================================================
-  // extension requirements
-  //===========================================================================
-
+    /*
+     * Extension requirements.
+     */
+    
     public String getExtensionName() {
-      return "sock1";
+        return "sock2";
     }
 
     public String getNLTypeName() {
-      // following comment plagarised from NetLogo table extension...
-	  // since this extension only defines one type, we don't
-      // need to give it a name; "table:" is enough,
-      // "table:table" would be redundant
-      return "";
+        /*
+         * Following comment taken from NetLogo table extension. Since this 
+         * extension only defines one type, we don't need to give it a name; 
+         * "table:" is enough, "table:table" would be redundant.
+         */
+        return "";
     }
 
-   // exportWorld & importWorld not specified because there is no persistent data
+    /* 
+     * The exportWorld and importWorld methods not specified because there is no 
+     * persistent data.
+     */
 
+    /*
+     * Extension primitives.
+     */
+    
+    /**
+     * Connects to a socket via a served port number, return value indicates 
+     * success/failure.
+     */
+    public static class ConnectLocal extends DefaultCommand
+    {
+        @Override
+        public Syntax getSyntax() {
+            return Syntax.commandSyntax(new int[]{Syntax.NumberType()});
+        }
 
+        @Override
+        public String getAgentClassString() { 
+            return "OTPL"; 
+        }
 
-//===========================================================================
-// extension primitives
-//===========================================================================
-
-//--- Boolean connect( int portNo ) ---------------------------------------
-
-/** connect to a socket via a served port number, return value indicates success/failure */
-public static class ConnectLocal extends DefaultCommand
-{
-    @Override
-    public Syntax getSyntax() {
-      return Syntax.commandSyntax
-          (new int[]{Syntax.NumberType()});
+        @Override
+        public void perform(Argument args[], Context context) 
+                throws ExtensionException, LogoException {
+            int port = (int)args[0].getIntValue();
+            comm = new NetlogoSocketIOStub(port, SocketConnection.CLIENT);
+        }
     }
 
-    @Override
-    public String getAgentClassString() { return "OTPL"; }
+    public static class ConnectDist extends DefaultCommand
+    {
+        @Override
+        public Syntax getSyntax() {
+          return Syntax.commandSyntax(new int[]{Syntax.StringType(), 
+              Syntax.NumberType()});
+        }
 
-    @Override
-    public void perform(Argument args[], Context context)
-        throws ExtensionException, LogoException {
-      int port = (int)args[0].getIntValue();
-	  comm = new NetlogoSocketIOStub(port, SocketConnection.CLIENT);
-    }
-}
+        @Override
+        public String getAgentClassString() { 
+            return "OTPL"; 
+        }
 
-/** connect to a socket via a served port number, return value indicates success/failure */
-public static class ConnectDist extends DefaultCommand
-{
-    public Syntax getSyntax() {
-      return Syntax.commandSyntax
-          (new int[]{Syntax.StringType(), Syntax.NumberType()});
-    }
-
-    public String getAgentClassString() { return "OTPL"; }
-
-    public void perform(Argument args[], Context context)
-        throws ExtensionException, LogoException {
-      String address = (String)args[0].get();
-	  int    port    = (int)args[1].getIntValue();
-	  comm = new NetlogoSocketIOStub(address, port, SocketConnection.CLIENT);
-    }
-}
-
-/** connect to a socket via a served port number, return value indicates success/failure */
-public static class Advertise extends DefaultCommand
-{
-    public Syntax getSyntax() {
-      return Syntax.commandSyntax
-          (new int[]{Syntax.NumberType()});
+        @Override
+        public void perform(Argument args[], Context context)
+                throws ExtensionException, LogoException {
+            String address = (String)args[0].get();
+            int port = (int)args[1].getIntValue();
+            comm = new NetlogoSocketIOStub(address, port,
+                    SocketConnection.CLIENT);
+        }
     }
 
-    public String getAgentClassString() { return "OTPL"; }
+    public static class Advertise extends DefaultCommand
+    {
+        @Override
+        public Syntax getSyntax() {
+          return Syntax.commandSyntax(new int[]{Syntax.NumberType()});
+        }
 
-    public void perform(Argument args[], Context context)
-        throws ExtensionException, LogoException {
-      int port = (int)args[0].getIntValue();
-	  comm = new NetlogoSocketIOStub(port, SocketConnection.SERVER);
-    }
-}
+        @Override
+        public String getAgentClassString() { 
+            return "OTPL"; 
+        }
 
-//--- void clear() --------------------------------
-
-/** clear any waiting input from the socket connection */
-public static class Clear extends DefaultCommand
-{
-    public Syntax getSyntax() {
-      return Syntax.commandSyntax
-          (new int[]{});
-    }
-
-    public String getAgentClassString() { return "OTPL"; }
-
-    public void perform(Argument args[], Context context)
-        throws ExtensionException, LogoException {
-      comm.clear();
-    }
-}
-
-
-//--- String peek() --------------------------------
-
-/**
- * Retrieves, but does not remove, the next line of input from the socket.
- * Returns null if there is no input.
- */
-public static class Peek extends DefaultReporter
-{
-    public Syntax getSyntax() {
-      return Syntax.reporterSyntax
-          (new int[]{},
-              Syntax.StringType());
+        @Override
+        public void perform(Argument args[], Context context)
+                throws ExtensionException, LogoException {
+            int port = (int)args[0].getIntValue();
+            comm = new NetlogoSocketIOStub(port, SocketConnection.SERVER);
+        }
     }
 
-    public String getAgentClassString() { return "OTPL"; }
+    /**
+     * Clears any waiting input from the socket connection.
+     */
+    public static class Clear extends DefaultCommand
+    {
+        @Override
+        public Syntax getSyntax() {
+            return Syntax.commandSyntax(new int[]{});
+        }
 
-    public Object report(Argument args[], Context context)
-        throws ExtensionException, LogoException {
-      return comm.peek();
-    }
-}
+        @Override
+        public String getAgentClassString() { 
+            return "OTPL"; 
+        }
 
-//--- String poll() --------------------------------
-
-/**
- * Retrieves and removes the next input line from the socket.
- * Returns null if there is no input. 
- */
-public static class Poll extends DefaultReporter
-{
-    public Syntax getSyntax() {
-      return Syntax.reporterSyntax
-          (new int[]{},
-              Syntax.StringType());
-    }
-
-    public String getAgentClassString() { return "OTPL"; }
-
-    public Object report(Argument args[], Context context)
-        throws ExtensionException, LogoException {
-      return comm.poll();
-    }
-}
-
-//--- String take() --------------------------------
-
-/**
- * Retrieves and removes the next input line from the socket.
- * Waits if there is no input. 
- */
-public static class Take extends DefaultReporter
-{
-    public Syntax getSyntax() {
-      return Syntax.reporterSyntax
-          (new int[]{},
-              Syntax.StringType());
+        @Override
+        public void perform(Argument args[], Context context)
+                throws ExtensionException, LogoException {
+            comm.clear();
+        }
     }
 
-    public String getAgentClassString() { return "OTPL"; }
+    /**
+     * Retrieves, but does not remove, the next line of input from the socket.
+     * Returns null if there is no input.
+     */
+    public static class Peek extends DefaultReporter
+    {
+        @Override
+        public Syntax getSyntax() {
+            return Syntax.reporterSyntax(new int[]{}, Syntax.StringType());
+        }
 
-    public Object report(Argument args[], Context context)
-        throws ExtensionException, LogoException {
-      return comm.take();
-    }
-}
+        @Override
+        public String getAgentClassString() { 
+            return "OTPL"; 
+        }
 
-
-//--- boolean isConnected() --------------------------------
-
-/**
- * Returns true/false if the socket is connected 
- */
-public static class IsConnected extends DefaultReporter
-{
-    public Syntax getSyntax() {
-      return Syntax.reporterSyntax
-          (new int[]{},
-              Syntax.BooleanType());
-    }
-
-    public String getAgentClassString() { return "OTPL"; }
-
-    public Object report(Argument args[], Context context)
-        throws ExtensionException, LogoException {
-      return comm.isConnected();
-    }
-}
-
-	
-	//--- boolean isEmpty() --------------------------------
-
-/**
- * Returns true/false if the socket is connected 
- */
-public static class IsEmpty extends DefaultReporter
-{
-    public Syntax getSyntax() {
-      return Syntax.reporterSyntax
-          (new int[]{},
-              Syntax.BooleanType());
+        @Override
+        public Object report(Argument args[], Context context)
+                throws ExtensionException, LogoException {
+            return comm.peek();
+        }
     }
 
-    public String getAgentClassString() { return "OTPL"; }
+    /**
+     * Retrieves and removes the next input line from the socket. Returns null 
+     * if there is no input. 
+     */
+    public static class Poll extends DefaultReporter
+    {
+        @Override
+        public Syntax getSyntax() {
+            return Syntax.reporterSyntax(new int[]{}, Syntax.StringType());
+        }
 
-    public Object report(Argument args[], Context context)
-        throws ExtensionException, LogoException {
-      return comm.isEmpty();
-    }
-}
+        @Override
+        public String getAgentClassString() { 
+            return "OTPL"; 
+        }
 
-
-//--- void write( String msg ) --------------------------------
-
-/** write a line of text onto the socket */
-public static class Write extends DefaultCommand
-{
-    public Syntax getSyntax() {
-      return Syntax.commandSyntax
-          (new int[]{Syntax.StringType()});
-    }
-
-    public String getAgentClassString() { return "OTPL"; }
-
-    public void perform(Argument args[], Context context)
-        throws ExtensionException, LogoException {
-      String str = (String)args[0].get();
-	  comm.write(str);
-    }
-}
-
-/** write a line of text onto the socket */
-public static class Disconnect extends DefaultCommand
-{
-    public Syntax getSyntax() {
-      return Syntax.commandSyntax
-          (new int[]{});
+        @Override
+        public Object report(Argument args[], Context context)
+                throws ExtensionException, LogoException {
+            return comm.poll();
+        }
     }
 
-    public String getAgentClassString() { return "OTPL"; }
+    /**
+     * Retrieves and removes the next input line from the socket. Waits if there 
+     * is no input. 
+     */
+    public static class Take extends DefaultReporter
+    {
+        @Override
+        public Syntax getSyntax() {
+            return Syntax.reporterSyntax(new int[]{}, Syntax.StringType());
+        }
+        
+        @Override
+        public String getAgentClassString() { 
+            return "OTPL"; 
+        }
 
-    public void perform(Argument args[], Context context)
-        throws ExtensionException, LogoException {
-      comm.close();
+        @Override
+        public Object report(Argument args[], Context context)
+                throws ExtensionException, LogoException {
+            return comm.take();
+        }
     }
-}
+
+    /**
+     * Returns true if the socket is connected, otherwise returns false.
+     */
+    public static class IsConnected extends DefaultReporter
+    {
+        @Override
+        public Syntax getSyntax() {
+            return Syntax.reporterSyntax(new int[]{}, Syntax.BooleanType());
+        }
+
+        @Override
+        public String getAgentClassString() { 
+            return "OTPL"; 
+        }
+
+        @Override
+        public Object report(Argument args[], Context context)
+                throws ExtensionException, LogoException {
+            return comm.isConnected();
+        }
+    }
+
+    public static class IsEmpty extends DefaultReporter
+    {
+        @Override
+        public Syntax getSyntax() {
+            return Syntax.reporterSyntax(new int[]{}, Syntax.BooleanType());
+        }
+
+        @Override
+        public String getAgentClassString() { 
+            return "OTPL"; 
+        }
+
+        @Override
+        public Object report(Argument args[], Context context)
+                throws ExtensionException, LogoException {
+            return comm.isEmpty();
+        }
+    }
+
+    /**
+     * Writes a line of text onto the socket.
+     */
+    public static class Write extends DefaultCommand
+    {
+        @Override
+        public Syntax getSyntax() {
+            return Syntax.commandSyntax(new int[]{Syntax.StringType()});
+        }
+
+        @Override
+        public String getAgentClassString() { 
+            return "OTPL"; 
+        }
+
+        @Override
+        public void perform(Argument args[], Context context)
+                throws ExtensionException, LogoException {
+            String str = (String)args[0].get();
+            comm.write(str);
+        }
+    }
+
+    public static class Disconnect extends DefaultCommand
+    {
+        @Override
+        public Syntax getSyntax() {
+            return Syntax.commandSyntax(new int[]{});
+        }
+
+        @Override
+        public String getAgentClassString() { 
+            return "OTPL"; 
+        }
+
+        @Override
+        public void perform(Argument args[], Context context)
+                throws ExtensionException, LogoException {
+            comm.close();
+        }
+    }
 }
